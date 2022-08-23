41a5ed57f8ade14e24407da58cea720a4b375fa3
ChillingPepper <90042155+ChillingPepper@users.noreply.github.com>
2022-08-23 16:59:13 +0200

    Keep retrying

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ from update import update_data
 if __name__ == "__main__":
     old_sovietscloset = SovietsCloset()
 
-    try:
-        update_data()
-    except:
-        print("something went wrong updating the data")
-        sleep_minutes = 5
-        for i in range(sleep_minutes):
-            print(f"trying again in {sleep_minutes - i} minutes")
-            time.sleep(60)
-        print("trying again now")
-        update_data()
+    updated = False
+    while not updated:
+        try:
+            update_data()
+            updated = True
+        except:
+            print("something went wrong updating the data")
+            sleep_minutes = 5
+            print(f"trying again in {sleep_minutes} minutes")
+            time.sleep(sleep_minutes * 60)
+            print("trying again now")
 
     new_sovietscloset = SovietsCloset()
     update_changelog(old_sovietscloset, new_sovietscloset)
